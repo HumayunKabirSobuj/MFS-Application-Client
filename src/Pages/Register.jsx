ddcb779ef400b6144ee061b942549433494c3156
humayunkabir506034 <humayunkabir506034@gmail.com>
2024-07-16 21:04:08 +0600

    register from create done

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -12,6 +12,7 @@ const Register = () => {
         navigate('/register/agent');
     };
 
+    
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
             <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
